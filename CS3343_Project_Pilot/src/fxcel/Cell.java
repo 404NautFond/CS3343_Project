aaaa5be43c74250e243c723f1017e62b2f3b24cd
lyy <lyy@lyydeMacBook-Pro-2.local>
2018-10-15 14:34:34 +0800

    Newer version

--- CS3343_Project_Pilot/src/fxcel/Cell.java
+++ CS3343_Project_Pilot/src/fxcel/Cell.java
@@ -8,98 +8,105 @@ import fxcelException.*;
 
 public class Cell extends Subject implements Observer {
 
-    /**
-     * For serialization
-     */
-    private static final long serialVersionUID = 1088607801192340601L;
-    private StringTokenizer myToken;
-    private String expression;
-    private double value;	//default is 0 when computing, but not display
-    private List<Cell> dependent;
-    /**
-     * Default Constructor
-     */
-    protected Cell() {
-    		this.expression = null;
-    		//this.cal = new Calculator();
-    }
-    
-    protected void assign(String expression) {
-    		char identifier = expression.charAt(0);
-    		if(identifier == ':')
-    			this.expression = expression;
-    		else if(identifier == '=');
-    			//TODO: check dependency
-    		else this.expression = expression;
-    }
-    
-    protected boolean checkDep(Cell c) {
-    		for(Cell dep:dependent) {
-    			if(dep == c) {
-    				if(this != c) return false;
-    			}
-    			if(!dep.checkDep(c)) return false;
-    		}
-    		return true;
-    }
-    
-    protected void clear() {
-    	
-    }
-    
-    @Override
-    public String toString() {
-    		return null;
-    }
-    
-    /**
-     * Constructor with String input
-     *
-     * @param input the intended content
-     */
-    protected void writeCell(String input) {
-        this.expression = input;
-    }
-    
-    protected String readContent() {
-    		return this.expression;
-    }
-    protected double readVal() {
-    		try {
-				computeVal();
-			} catch (FxcelException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
+	/**
+	 * For serialization
+	 */
+	private static final long serialVersionUID = 1088607801192340601L;
+	private StringTokenizer myToken;
+	private String expression;
+	private double value;	//default is 0 when computing, but not display
+	private List<Cell> dependent;
+	/**
+	 * Default Constructor
+	 */
+	protected Cell() {
+		this.expression = null;
+		//this.cal = new Calculator();
+	}
+
+	protected void assign(String expression) {
+		char identifier = expression.charAt(0);
+		if(identifier == ':')
+			this.expression = expression;
+		else if(identifier == '=');
+		//TODO: check dependency
+		else this.expression = expression;
+	}
+
+	protected boolean checkDep(Cell c) {
+		for(Cell dep:dependent) {
+			if(dep == c) {
+				if(this != c) return false;
 			}
-    		return this.value;
-    }
+			if(!dep.checkDep(c)) return false;
+		}
+		return true;
+	}
 
-    private Cell findCell(String loc) {
-        int row = loc.charAt(0) - 'A';
-        int col = Integer.parseInt(loc.substring(1));	//TODO:?
-        return Fxcel.getInstance().getCell(row, col);
-    }
+	protected void clear() {
 
-    /**
-     * Compute the value of the content included in the cell
-     *
-     * @return the value of the cell, if not a number, return null
-     */
-//    protected double computeVal() throws FxcelException {
-//    		try {
-//    			value = Double.parseDouble(expression);
-//    		}catch(NumberFormatException e) {
-//    	        if (expression.charAt(0) == '=') {
-//    	            myToken = new StringTokenizer(expression, "=+-*/%^()!", true);
-//    	            //token 1 can be Cell Location and Function Name
-//    	            this.value = Calculator.computeToken(myToken);
-//    	        } else 
-//    	        	throw new NonParsableException();
-//    		}
-//        return value;
-//    }
-    
-    public int getColumn() {
-        return 0;
-    }
+	}
+
+	@Override
+	public String toString() {
+		return null;
+	}
+
+	/**
+	 * Constructor with String input
+	 *
+	 * @param input the intended content
+	 */
+	protected void writeCell(String input) {
+		this.expression = input;
+	}
+
+	protected String readContent() {
+		return this.expression;
+	}
+	//    protected double readVal() {
+	//    		try {
+	//				computeVal();
+	//			} catch (FxcelException e) {
+	//				// TODO Auto-generated catch block
+	//				e.printStackTrace();
+	//			}
+	//    		return this.value;
+	//    }
+	//
+	//    private Cell findCell(String loc) {
+	//        int row = loc.charAt(0) - 'A';
+	//        int col = Integer.parseInt(loc.substring(1));	//TODO:?
+	//        return Fxcel.getInstance().getCell(row, col);
+	//    }
+	//
+	//    /**
+	//     * Compute the value of the content included in the cell
+	//     *
+	//     * @return the value of the cell, if not a number, return null
+	//     */
+	//    protected double computeVal() throws FxcelException {
+	//    		try {
+	//    			value = Double.parseDouble(expression);
+	//    		}catch(NumberFormatException e) {
+	//    	        if (expression.charAt(0) == '=') {
+	//    	            myToken = new StringTokenizer(expression, "=+-*/%^()!", true);
+	//    	            //token 1 can be Cell Location and Function Name
+	//    	            this.value = Calculator.computeToken(myToken);
+	//    	        } else 
+	//    	        	throw new NonParsableException();
+	//    		}
+	//        return value;
+	//    }
+
+	public int getColumn() {
+		return 0;
+	}
+
+	@Override
+	public void update() {
+		// TODO Auto-generated method stub
+
+	}
+
 }
--- CS3343_Project_Pilot/src/fxcel/Observer.java
+++ CS3343_Project_Pilot/src/fxcel/Observer.java
@@ -1,27 +1,5 @@
 package fxcel;
 
-import java.util.ArrayList;
-
 public interface Observer{
-		private ArrayList<Cell> subscriber = new ArrayList<Cell>();
-		public Observer() {}
-		public boolean subscribe(Cell c) {
-			try {
-				subscriber.add(c);
-				return true;
-			} catch (Exception e) {
-				return false;
-			}
-		}
-		public boolean updateAll() {
-			try {
-				for(Cell c:subscriber) {
-					c.computeVal();
-				}
-				return true;
-			} catch (Exception e) {
-				return false;
-			}
-		}
-	};
+	void update();
 }
