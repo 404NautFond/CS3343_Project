40190d6a0f0e0bccab68a451f044464b86955e05
LI Yuyang <lyybsni@gmail.com>
2018-10-22 15:23:08 +0800

    Write the JavaDoc for General Handler

--- CS3343_Project_Pilot/src/fxcelHandler/GeneralHandler.java
+++ CS3343_Project_Pilot/src/fxcelHandler/GeneralHandler.java
@@ -2,7 +2,7 @@ package fxcelHandler;
 
 import java.util.ArrayList;
 import java.util.EmptyStackException;
-import java.util.NoSuchElementException;
+//import java.util.NoSuchElementException;
 import java.util.Stack;
 import java.util.StringTokenizer;
 
@@ -40,7 +40,7 @@ public class GeneralHandler extends ExpHandler{
 					case "/":
 					case "^":
 						number2 = tokens.remove(0);
-						number2 = (number2.equals("("))?(recursion()):(expandOperand(number2));
+						number2 = expand(number2);
 						sym = tempToken;
 						number1 = buffer.pop();
 						buffer.push(calcu(number1, sym, number2)+"");
@@ -50,15 +50,16 @@ public class GeneralHandler extends ExpHandler{
 					case "-":
 						buffer.push(tempToken);
 						break;
-					/* syntax error detected */
-					case "(":
-						buffer.push(""+recursion());
-						break;
-					/* closure of parenthesis */
+					/* recursion, to be included in the expand */
+//					case "(":
+//						buffer.push(recursion());
+//						break;
+					/* syntax error */
 					case ")":
 						throw new InvalidExpressionException();
+					/* operands other than above */
 					default:
-						buffer.push(expandOperand(tempToken));
+						buffer.push(expand(tempToken));
 					}
 				}catch(InvalidCellException e) {
 					buffer.push("0");
@@ -73,7 +74,6 @@ public class GeneralHandler extends ExpHandler{
 		return Double.parseDouble(buffer.lastElement());
 	}
 	
-	
 	/**
 	 * Initialize the handler by the expression
 	 */
@@ -85,83 +85,12 @@ public class GeneralHandler extends ExpHandler{
 	}
 	
 	/**
-	 * Process the parenthesis content
-	 * @return The only output token
-	 * @throws InvalidExpressionException
+	 * Process the basic operations
+	 * @param number1 The number before the sym
+	 * @param sym The type of operation
+	 * @param number2 The number after the sym
+	 * @return The value of the operation
 	 */
-	private String recursion() throws InvalidExpressionException{
-		String temp = "";
-		if(tokens.size() == 0) throw new InvalidExpressionException();
-		while(!tokens.get(0).equals(")")) {
-			temp = temp + tokens.remove(0);
-		}
-		tokens.remove(0);
-		double res = -1;
-		try {
-			GeneralHandler tempHandler = new GeneralHandler();
-			res = tempHandler.handle(temp);
-//						System.out.println("The recursion output is: " + res);
-		} catch (NoSuchElementException e) {
-			throw new InvalidExpressionException();
-		} catch (EmptyStackException e) {
-			throw new InvalidExpressionException();
-		}
-		return ""+res;
-	}
-	
-	private void lowestPriority() {
-		String sym, number1, number2;
-		while(buffer.size() > 1) {
-			number2 = buffer.pop();
-			sym = buffer.pop();
-			try {
-				number1 = buffer.pop();
-				if(sym.equals("-")) {
-					if(!ExpHandler.isNumeric(number1)) {
-						buffer.push(number1);
-						number1 = "0";
-					}					
-				}
-			}catch(EmptyStackException e) {
-				number1 = "0";
-			}
-			buffer.push(calcu(number1, sym, number2)+"");
-		}
-	}
-
-	private String expandOperand(String operand) 
-			throws InvalidCellException, InvalidExpressionException {
-		if(isNumeric(operand)) {
-			return operand;
-		}else if(isCell(operand)) {
-			Cell targetCell = Fxcel.getInstance().getCell(operand);
-			return (targetCell.getValue()+"");
-		}else {
-			return expandFormula(operand,tokens);
-		}
-	}
-	
-	private String expandFormula(String formula, ArrayList<String> tokens) 
-			throws InvalidExpressionException {
-		
-		String tempExpr = "";
-	
-		try {
-			while(!tokens.get(0).equals(")")) {
-				tempExpr = tempExpr + tokens.remove(0);
-			}
-			tokens.remove(0);
-		} catch(IndexOutOfBoundsException e) {
-			throw new InvalidExpressionException();
-		}
-		
-		switch(formula) {
-		case "SUM":
-			return ""+new SumHandler().handle(tempExpr);
-		}
-		return null;
-	}
-
 	public double calcu(String number1, String sym, String number2) {
 		double num1 = Double.parseDouble(number1);
 		double num2 = Double.parseDouble(number2);
@@ -181,5 +110,110 @@ public class GeneralHandler extends ExpHandler{
 		}
 	}
 
+	/**
+	 * Process the parenthesis content
+	 * @return The only output token
+	 * @throws InvalidExpressionException
+	 */
+	private String recursion() throws InvalidExpressionException{
+		String temp = "";
 
+		/* Syntax error detected */
+		if(tokens.size() == 0) throw new InvalidExpressionException();
+		
+		// add parameters back to string
+		while(!tokens.get(0).equals(")")) {
+			temp = temp + tokens.remove(0);
+		}
+		tokens.remove(0);			// remove the ")"
+		
+		// define the output to 0
+		double res = 0;
+		try {
+			// recursively call the handler
+			GeneralHandler tempHandler = new GeneralHandler();
+			res = tempHandler.handle(temp);
+//		} catch (NoSuchElementException e) {
+//			throw new InvalidExpressionException();
+		} catch (EmptyStackException e) {
+			throw new InvalidExpressionException();
+		}
+		return ""+res;
+	}
+
+	/**
+	 * Process the level 1 priority operators
+	 */
+	private void lowestPriority() {
+		String sym, number1, number2;
+		// post-condition: only one token left
+		while(buffer.size() > 1) {
+			number2 = buffer.pop();
+			sym = buffer.pop();
+			try {
+				number1 = buffer.pop();
+				// specially handle negative values
+				// TODO =-1+5
+				if(sym.equals("-")) {
+					if(!ExpHandler.isNumeric(number1)) {
+						buffer.push(number1);
+						number1 = "0";
+					}					
+				}
+			}catch(EmptyStackException e) {
+				number1 = "0";
+			}
+			buffer.push(calcu(number1, sym, number2)+"");
+		}
+	}
+
+	/**
+	 * Process the function content
+	 * @param formula The formula keyword
+	 * @param tokens The related parameters
+	 * @return The function value
+	 * @throws InvalidExpressionException
+	 */
+	private String compute(String formula, ArrayList<String> tokens) 
+			throws InvalidExpressionException {
+		// Gathering parameters
+		String tempExpr = "";
+		try {
+			while(!tokens.get(0).equals(")")) {
+				tempExpr = tempExpr + tokens.remove(0);
+			}
+			tokens.remove(0);
+		} catch(IndexOutOfBoundsException e) {
+			throw new InvalidExpressionException();
+		}
+		// Choosing formula type
+		switch(formula) {
+		case "SUM":
+			return ""+new SumHandler().handle(tempExpr);
+		}
+		// If not match anything
+		throw new InvalidExpressionException();
+	}
+
+	/**
+	 * Process the Cell references, the Function references, and recursions
+	 * @param operand The token to be evaluated
+	 * @return The real value the piece of token taken
+	 * @throws InvalidCellException
+	 * @throws InvalidExpressionException
+	 */
+	private String expand(String operand) 
+			throws InvalidCellException, InvalidExpressionException {
+		if(operand.equals("(")) {
+			return recursion();
+		}else if(isNumeric(operand)) {
+			return operand;
+		}else if(isCell(operand)) {
+			Cell targetCell = Fxcel.getInstance().getCell(operand);
+			return (targetCell.getValue()+"");
+		}else {
+			return compute(operand, tokens);
+		}
+	}
+	
 }
