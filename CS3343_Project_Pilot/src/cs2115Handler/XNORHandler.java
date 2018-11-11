767389ad345049aff3e09b4912a9ce43bd7e1025
jason-zzx <zxzhukke@gmail.com>
2018-11-11 10:59:32 +0000

    Update XNORHandler.java

--- CS3343_Project_Pilot/src/cs2115Handler/XNORHandler.java
+++ CS3343_Project_Pilot/src/cs2115Handler/XNORHandler.java
@@ -1,13 +1,28 @@
 package cs2115Handler;
 
+import fxcel.Fxcel;
+import fxcelException.InvalidCellException;
 import fxcelException.InvalidExpressionException;
 
-public class XNORHandler extends LogicHandler {
+public class XNORHandler extends LogicHandler{
 
 	@Override
 	public double handleForDoubleReturn(String expression) throws InvalidExpressionException {
-		// TODO Auto-generated method stub
-		return 0;
+		String[] input = expression.split(",");
+		int count = 0;
+		for(String celltext: input) {
+			try {
+				if(celltext.equals("TRUE") || !celltext.equals("0") || Fxcel.getInstance().getCellValue(celltext) != 0)
+					count++;
+			} catch (InvalidCellException e) {
+				e.printStackTrace();
+			}
+		}
+		if(count % 2 == 0) {
+			return 1;
+		}else {
+			return 0;
+		}
 	}
 
 }
