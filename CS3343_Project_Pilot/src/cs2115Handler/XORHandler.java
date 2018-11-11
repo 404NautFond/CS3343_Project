1fe946fbedc7193442bb68c51c00547dd12cb66d
jason-zzx <zxzhukke@gmail.com>
2018-11-11 10:59:46 +0000

    Update XORHandler.java

--- CS3343_Project_Pilot/src/cs2115Handler/XORHandler.java
+++ CS3343_Project_Pilot/src/cs2115Handler/XORHandler.java
@@ -1,13 +1,26 @@
-package cs2115Handler;
-
+import fxcel.Fxcel;
+import fxcelException.InvalidCellException;
 import fxcelException.InvalidExpressionException;
 
-public class XORHandler extends LogicHandler {
+public class XORHandler extends LogicHandler{
 
 	@Override
 	public double handleForDoubleReturn(String expression) throws InvalidExpressionException {
-		// TODO Auto-generated method stub
-		return 0;
+		String[] input = expression.split(",");
+		int count = 0;
+		for(String celltext: input) {
+			try {
+				if(celltext.equals("TRUE") || !celltext.equals("0") || Fxcel.getInstance().getCellValue(celltext) != 0)
+					count++;
+			} catch (InvalidCellException e) {
+				e.printStackTrace();
+			}
+		}
+		if(count % 2 == 0) {
+			return 0;
+		}else {
+			return 1;
+		}
 	}
 
 }
