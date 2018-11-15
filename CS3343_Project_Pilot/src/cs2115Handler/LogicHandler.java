c770ca4e87901327483322bcda757c8d312a3a05
jason-zzx <zxzhukke@gmail.com>
2018-11-15 08:59:15 +0000

    add comments

--- CS3343_Project_Pilot/src/cs2115Handler/LogicHandler.java
+++ CS3343_Project_Pilot/src/cs2115Handler/LogicHandler.java
@@ -6,14 +6,25 @@ import fxcelHandler.FuncHandler;
 import fxcelHandler.GeneralHandler;
 
 public abstract class LogicHandler extends FuncHandler{
-	//TODO: Assumption, TRUE: textual value = "TRUE", numerical value = 1.00;
+	//Assumption, TRUE: textual value = "TRUE", numerical value = 1.00;
 	//FALSE: textual value = "FALSE", numerical value = 0.00;
+        @Override
 	public abstract double handleForDoubleReturn(String expression) throws InvalidExpressionException;
 	
+        /**
+         * To detect if a token is Cell-name-like
+	 * @param str The token
+	 * @return boolean If the token is Cell name
+         */
 	public static boolean isCell(String str) {
 		return ExpHandler.isCell(str);
 	}
-	
+        
+        /**
+         * To detect if the expression is TRUE
+         * @param str input expression
+         * @return boolean the result of the expression
+         */
 	public static boolean isTrueLike(String str) {
 		if(ExpHandler.isNumeric(str) && !str.equals("0")) return true;
 		else if(str.equalsIgnoreCase("true")) return true;
@@ -21,7 +32,12 @@ public abstract class LogicHandler extends FuncHandler{
 		else if(new GeneralHandler().handleForDoubleReturn(str) != 0) return true;
 		else return false;
 	}
-	
+        
+	/**
+         * To detect if the expression is False
+         * @param str input expression
+         * @return boolean the result of the expression
+         */
 	public static boolean isFalseLike(String str) {
 		if(str.equals("0")) return true;
 		else if(str.equalsIgnoreCase("true")) return false;
