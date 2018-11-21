942ed68f0ddbd23be55b8c52bd830d709452266d
jason-zzx <zxzhukke@gmail.com>
2018-11-21 04:22:03 +0000

    debug

--- CS3343_Project_Pilot/src/cs2115Handler/XORHandler.java
+++ CS3343_Project_Pilot/src/cs2115Handler/XORHandler.java
@@ -6,7 +6,7 @@ public class XORHandler extends LogicHandler{
 
 	@Override
 	public double handleForDoubleReturn(String expression) throws InvalidExpressionException {
-		return new NOTHandler().handleForDoubleReturn(""+new XNORHandler().handleForDoubleReturn(expression));
+		return new NOTHandler().handleForDoubleReturn(""+(int)new XNORHandler().handleForDoubleReturn(expression));
 	}
 
 }
